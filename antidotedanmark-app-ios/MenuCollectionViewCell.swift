fa8e570acef14ca600e0dd6d1e5b1e7c267c3668
Casper Riboe <Casper@Caspers-MBP.domain_not_set.invalid>
2017-04-27 22:10:45 +0200

    Added basic menu layout

---
+++ antidotedanmark-app-ios/MenuCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MenuCollectionViewCell.swift
+//  antidotedanmark-app-ios
+//
+//  Created by Casper Riboe on 27/04/2017.
+//  Copyright © 2017 Criboe. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var titleLabel: UILabel!
+	fileprivate var link: URL?
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
+
+extension MenuCollectionViewCell {
+	public func configure(model: RSSItem) {
+		titleLabel.text = model.title
+		link = model.link
+	}
+	
+}
---
+++ antidotedanmark-app-ios/MenuViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MenuViewController.swift
+//  antidotedanmark-app-ios
+//
+//  Created by Casper Riboe on 27/04/2017.
+//  Copyright © 2017 Criboe. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+	@IBOutlet weak var collectionView: UICollectionView!
+	@IBOutlet weak var firstHelpButton: UIButton!
+	@IBOutlet weak var videoGuideButton: UIButton!
+	fileprivate let cellId = "cellId";
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		collectionView.delegate = self
+		collectionView.dataSource = self
+		collectionView.register(UINib(nibName: "MenuCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: cellId)
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+	
+	
+}
+
+extension MenuViewController: UICollectionViewDelegateFlowLayout {
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+		return 0
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+		return collectionView.bounds.size.width * 0.05
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+		let inset = collectionView.bounds.size.width * 0.05
+		return UIEdgeInsets(top: inset, left: 0, bottom: inset, right: 0)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let width: CGFloat = collectionView.bounds.size.width * 0.9
+		let height: CGFloat = 64
+		return CGSize(width: width, height: height)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MenuCollectionViewCell
+		
+		return cell
+	}
+
+}
+
+extension MenuViewController: UICollectionViewDataSource {
+	
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return 1
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 3
+	}
+	
+}
