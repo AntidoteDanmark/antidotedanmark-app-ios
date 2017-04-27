04dc37f193a1819784d1fe2a664955bfc4adedbf
Casper Riboe <Casper@Caspers-MBP.local>
2017-04-27 19:35:08 +0200

    Added Xcode project

---
+++ antidotedanmark-app-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  antidotedanmark-app-ios
+//
+//  Created by Casper Riboe on 27/04/2017.
+//  Copyright © 2017 Criboe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
