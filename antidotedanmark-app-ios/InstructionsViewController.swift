d1f76d0487cbb333a507bd12d4475f5fac89e01b
Casper Riboe <Casper@Caspers-MBP.domain_not_set.invalid>
2017-04-28 10:07:56 +0200

    Added hardcoded viewmodels

--- antidotedanmark-app-ios/InstructionsViewController.swift
+++ antidotedanmark-app-ios/InstructionsViewController.swift
@@ -13,6 +13,7 @@ class InstructionsViewController: UIViewController {
 	@IBOutlet weak var collectionView: UICollectionView!
 	@IBOutlet weak var pageControl: UIPageControl!
 	fileprivate let cellId = "cellId"
+	fileprivate var cellViewModels: [InstructionItemViewModel] = [] { didSet{ self.collectionView.reloadData() }}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -23,6 +24,19 @@ class InstructionsViewController: UIViewController {
 		collectionView.delegate = self
 		collectionView.dataSource = self
 		collectionView.register(UINib(nibName: "InstructionCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: cellId)
+		
+		let item1 = InstructionItemViewModel(image: UIImage(named: "step01")!, title: "Tegn På Overdosis", description: "Ingen/svag vejrtrækning. \nBlå læber og negle. \nIngen reaktion. \nRÅB OM HJÆLP!", step: 1, maxStep: 6)
+		let item2 = InstructionItemViewModel(image: UIImage(named: "step02")!, title: "Sikre Frie Luftveje", description: "Bøj hovedet tilbage og åben munden. \nLyt og føl efter vejrtrækning i max 10 sek. \nSe om brystet bevæger sig.", step: 2, maxStep: 6)
+		let item3 = InstructionItemViewModel(image: UIImage(named: "step03")!, title: "Ingen Reaktion Ring 1–1–2", description: "ER DER NORMAL VEJRTRÆKNING? \nNEJ -> HJERTESTOP! -> GÅ TIL 4 + 5. \nJA -> GÅ TIL 6.", step: 3, maxStep: 6)
+		let item4 = InstructionItemViewModel(image: UIImage(named: "step04")!, title: "Kunstigt Åndedræt + Hjertemassage", description: "Hjertemassage = 30 tryk \nIndblæsning = 2 forsøg \nGentag til hjælp kommer/normal vejrtrækning.", step: 4, maxStep: 6)
+		let item5 = InstructionItemViewModel(image: UIImage(named: "step05")!, title: "Modgift Næsespray", description: "HJÆLP HINANDEN! \n1 ampul i hvert næsebor - hvis det ikke forsinker hjertemassage/indblæsninger ved hjertestop.", step: 5, maxStep: 6)
+		let item6 = InstructionItemViewModel(image: UIImage(named: "step06")!, title: "Normal Vejrtrækning", description: "Læg personen på siden. \nSikre frie luftveje (2). \nGiv modgift ved tegn på overdosis. \nBliv til ambulancen kommer.", step: 6, maxStep: 6)
+		cellViewModels.append(item1)
+		cellViewModels.append(item2)
+		cellViewModels.append(item3)
+		cellViewModels.append(item4)
+		cellViewModels.append(item5)
+		cellViewModels.append(item6)
 	}
 	
 	override func didReceiveMemoryWarning() {
@@ -67,13 +81,15 @@ extension InstructionsViewController: UICollectionViewDelegateFlowLayout {
 extension InstructionsViewController: UICollectionViewDataSource {
 
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		let size = 6
+		let size = cellViewModels.count
 		pageControl.numberOfPages = size
 		return size
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! InstructionCollectionViewCell
+		
+		cell.configure(model: cellViewModels[indexPath.row])
 		
 		return cell
 	}
